b742b4c6d8775fbbcc19b89b5470f4a53c7c9390
Sonu <sonu.chapter247@gmail.com>
2019-04-04 20:26:59 +0530

    Login started

--- src/components/common/CrmCircleIcon.js
+++ src/components/common/CrmCircleIcon.js
@@ -1,25 +1,16 @@
 import React, { Component } from "react";
-import {
-  DropdownItem,
-  DropdownToggle,
-  DropdownMenu,
-  Badge,
-  Dropdown
-} from "reactstrap";
 
-class CrmCircleIcon
- extends Component {
+class CrmCircleIcon extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
-      const { circleIconPass, cssPass } = this.props;      
-      
+    const { circleIconPass, cssPass } = this.props;
+
     return (
-      <span style={cssPass ? cssPass: null} className="circle-icon-option">
+      <span style={cssPass ? cssPass : null} className="circle-icon-option">
         <i class={circleIconPass} />
       </span>
     );
