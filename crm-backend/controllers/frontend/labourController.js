b124a247586e28ec4521365ad6c976c74c89b567
Sonu <sonu.chapter247@gmail.com>
2019-04-10 16:11:44 +0530

    MInor Updates

--- crm-backend/controllers/frontend/labourController.js
+++ crm-backend/controllers/frontend/labourController.js
@@ -3,31 +3,33 @@ const rateStandardModel = require("../../models/rateStandard");
 const getAllStandardRate = async (req, res) => {
   try {
     let $data = req.body;
-    const getAllStdRate = await rateStandardModel.find({ parentId: $data.parentId });
+    const getAllStdRate = await rateStandardModel.find({
+      parentId: $data.parentId,
+    });
     if (getAllStdRate) {
       if (getAllStdRate.length) {
         return res.status(200).json({
           responsecode: 200,
           success: true,
-          data: getAllStdRate
+          data: getAllStdRate,
         });
       }
     } else {
       return res.status(400).json({
         responsecode: 200,
-        success: false
+        success: false,
       });
     }
   } catch (error) {
     res.status(500).json({
       responsecode: 500,
       message: error.message ? error.message : "Unexpected error occure.",
-      success: false
+      success: false,
     });
   }
 };
 /* -------------Get All Roles End------------ */
 
 module.exports = {
-  getAllStandardRate
+  getAllStandardRate,
 };
