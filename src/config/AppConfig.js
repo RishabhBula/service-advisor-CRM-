a15523bb57ee6d97b05b4f5fd46adb5fe3e88503
Sonu <sonu.chapter247@gmail.com>
2019-04-11 15:27:11 +0530

    Minor updates

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,16 +1,15 @@
-
 export const EnviornmentTypes = {
-DEV: "development",
-PROD: "production",
+  DEV: "development",
+  PROD: "production",
 };
 export const mode = process.env.NODE_ENV || EnviornmentTypes.DEV; //stage,dev,live
 export const isProd = mode === EnviornmentTypes.PROD;
 let data;
 data = {
-API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
-API_VERSION: process.env.REACT_APP_API_VERSION,
-phoneLength: 3,
-DEFAULT_DATE_FORMAT: "LLL",
-ITEMS_PER_PAGE: 5,
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+  API_VERSION: process.env.REACT_APP_API_VERSION,
+  phoneLength: 3,
+  DEFAULT_DATE_FORMAT: "LLL",
+  ITEMS_PER_PAGE: 5,
 };
 export const AppConfig = data;
