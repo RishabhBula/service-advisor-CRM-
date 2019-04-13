6e6fee5a6f2e688f897890e405d42f09dfcd35e3
Tushar Goyal <tushar.chapter247@gmail.com>
2019-04-13 19:58:03 +0530

    implement multiselect on customer table. working on functionality implementation

--- src/components/Customer/CustomerList/index.js
+++ src/components/Customer/CustomerList/index.js
@@ -20,16 +20,19 @@ import * as qs from "query-string";
 import { AppConfig } from "../../../config/AppConfig";
 import { ConfirmBox } from "../../../helpers/SweetAlert";
 import { CrmUserModal } from "../../common/CrmUserModal";
+import { toast } from 'react-toastify';
+
 class CustomerList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       page: 1,
-      search: "",
-      status: "",
-      sort: "",
+      search: '',
+      status: '',
+      sort: '',
       user: {},
-      openEditModal: false
+      openEditModal: false,
+      selectedCustomers: [],
     };
   }
   componentDidMount() {
@@ -38,21 +41,116 @@ class CustomerList extends Component {
     const { page, search, sort, status } = qs.parse(lSearch);
     this.setState({
       page: parseInt(page) || 1,
-      sort: sort || "",
-      status: status || "",
-      search: search || ""
+      sort: sort || '',
+      status: status || '',
+      search: search || '',
     });
   }
   componentDidUpdate({ openEdit }) {
     if (this.props.openEdit !== openEdit) {
       this.setState({
-        openEditModal: false
+        openEditModal: false,
       });
     }
   }
+  handleCheckAllCheckBox = e => {
+    const { customerData } = this.props;
+    const { customers } = customerData;
+    const { target } = e;
+    const { checked } = target;
+    if (!checked) {
+      this.setState({
+        selectedCustomers: [],
+      });
+      return;
+    }
+    const selectedCustomers = [];
+    customers.forEach(user => {
+      selectedCustomers.push(user._id);
+    });
+    this.setState({ selectedCustomers });
+  };
+  handleCheckboxChnage = e => {
+    const { target } = e;
+    const { checked, value } = target;
+    const { selectedCustomers } = this.state;
+    if (checked) {
+      selectedCustomers.push(value);
+      this.setState({
+        selectedCustomers,
+      });
+      return;
+    }
+    const index = selectedCustomers.indexOf(value);
+    selectedCustomers.splice(index, 1);
+    this.setState({
+      selectedCustomers,
+    });
+  };
+  handleActionChange = e => {
+    const { selectedCustomers } = this.state;
+    const { target } = e;
+    const { value } = target;
+    if (!value) {
+      return;
+    }
+    if (!selectedCustomers.length) {
+      toast.error('Please select at least one customer.');
+      return;
+    }
+    if (value === 'active') {
+      this.activateUsers(true);
+    } else if (value === 'inactive') {
+      this.deactivateUsers(true);
+    } else if (value === 'delete') {
+      this.onDelete(true);
+    }
+  };
+  activateUsers = async (isMultiple = false) => {
+    const { value } = await ConfirmBox({
+      text: isMultiple
+        ? 'Do you want to active selected customer(s)?'
+        : 'Do you want to active this customer?',
+    });
+    if (!value) {
+      this.setState({
+        selectedCustomers: [],
+      });
+      return;
+    }
+    this.props.onStatusUpdate({ status: 1, users: this.state.selectedCustomers });
+  };
+  deactivateUsers = async (isMultiple = false) => {
+    const { value } = await ConfirmBox({
+      text: isMultiple
+        ? 'Do you want to inactive selected customer(s)?'
+        : 'Do you want to inactive this customer?',
+    });
+    if (!value) {
+      this.setState({
+        selectedCustomers: [],
+      });
+      return;
+    }
+    this.props.onStatusUpdate({ status: 0, users: this.state.selectedCustomers });
+  };
+  onDelete = async (isMultiple = false) => {
+    const { value } = await ConfirmBox({
+      text: isMultiple
+        ? "Do you want to delete selected customer(s)?"
+        : "Do you want to delete this customer?"
+    });
+    if (!value) {
+      this.setState({
+        selectedCustomers: []
+      });
+      return;
+    }
+    this.props.onDelete(this.state.selectedCustomers);
+  };
   handleChange = e => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
   onSearch = e => {
@@ -63,7 +161,7 @@ class CustomerList extends Component {
       param.page = page;
     }
     if (search) {
-      param.search = search.trim(" ");
+      param.search = search.trim(' ');
     }
     if (sort) {
       param.sort = sort;
@@ -77,16 +175,27 @@ class CustomerList extends Component {
     e.preventDefault();
     this.setState({
       page: 1,
-      search: "",
-      status: "",
-      sort: "",
-      user: {}
+      search: '',
+      status: '',
+      sort: '',
+      user: {},
     });
     this.props.onSearch({});
   };
+
+  changeStatus = async (e, customerId) => {
+    const { value } = await ConfirmBox({
+      text: 'Do you want to update the status of this customer?',
+    });
+    if (!value) {
+      return;
+    }
+    this.props.changeStatus(e, customerId);
+  };
+
   onDelete = async userId => {
     const { value } = await ConfirmBox({
-      text: "Do you want to delete this customer?"
+      text: 'Do you want to delete this customer?',
     });
     if (!value) {
       return;
@@ -94,7 +203,7 @@ class CustomerList extends Component {
     this.props.onDelete(userId);
   };
   editUser = customer => {
-    this.props.updateModel(customer)
+    this.props.updateModel(customer);
   };
   onUpdate = (id, data) => {
     this.props.onUpdate(id, data);
@@ -102,41 +211,49 @@ class CustomerList extends Component {
   render() {
     const { customerData } = this.props;
     const { customers, isLoading, totalCustomers } = customerData;
-    const { page, search, sort, status, user, openEditModal } = this.state;
+    const {
+      page,
+      search,
+      sort,
+      status,
+      user,
+      openEditModal,
+      selectedCustomers,
+    } = this.state;
     return (
       <>
-        <div className={"filter-block"}>
+        <div className={'filter-block'}>
           <Form onSubmit={this.onSearch}>
             <Row>
-              <Col lg={"4"} md={"4"} className="mb-0">
-                <FormGroup className="mb-0">
-                  <Label className="label">Search</Label>
-                  <InputGroup className="mb-2">
+              <Col lg={'4'} md={'4'} className='mb-0'>
+                <FormGroup className='mb-0'>
+                  <Label className='label'>Search</Label>
+                  <InputGroup className='mb-2'>
                     <input
-                      type="text"
-                      name="search"
+                      type='text'
+                      name='search'
                       onChange={this.handleChange}
-                      className="form-control"
+                      className='form-control'
                       value={search}
-                      aria-describedby="searchUser"
-                      placeholder="Search by first name, last name and email"
+                      aria-describedby='searchUser'
+                      placeholder='Search by first name, last name and email'
                     />
                   </InputGroup>
                 </FormGroup>
               </Col>
-              <Col lg={"3"} md={"3"} className="mb-0">
-                <FormGroup className="mb-0">
-                  <Label for="exampleSelect" className="label">
+              <Col lg={'3'} md={'3'} className='mb-0'>
+                <FormGroup className='mb-0'>
+                  <Label for='exampleSelect' className='label'>
                     Status
                   </Label>
                   <Input
-                    type="select"
-                    name="status"
-                    id="exampleSelect"
+                    type='select'
+                    name='status'
+                    id='exampleSelect'
                     onChange={this.handleChange}
                     value={status}
                   >
-                    <option className="form-control" value={""}>
+                    <option className='form-control' value={''}>
                       -- Select Status --
                     </option>
                     <option value={1}>Active</option>
@@ -144,53 +261,53 @@ class CustomerList extends Component {
                   </Input>
                 </FormGroup>
               </Col>
-              <Col lg={"3"} md={"3"} className="mb-0">
-                <FormGroup className="mb-0">
-                  <Label for="SortFilter" className="label">
+              <Col lg={'3'} md={'3'} className='mb-0'>
+                <FormGroup className='mb-0'>
+                  <Label for='SortFilter' className='label'>
                     Sort By
                   </Label>
                   <Input
-                    type="select"
-                    name="sort"
-                    id="SortFilter"
+                    type='select'
+                    name='sort'
+                    id='SortFilter'
                     onChange={this.handleChange}
                     value={sort}
                   >
-                    <option className="form-control" value={""}>
+                    <option className='form-control' value={''}>
                       -- Select Status --
                     </option>
-                    <option value={"createddesc"}>Last Created</option>
-                    <option value={"nasc"}>Name A-Z</option>
-                    <option value={"ndesc"}>Name Z-A</option>
+                    <option value={'createddesc'}>Last Created</option>
+                    <option value={'nasc'}>Name A-Z</option>
+                    <option value={'ndesc'}>Name Z-A</option>
                   </Input>
                 </FormGroup>
               </Col>
-              <Col lg={"2"} md={"2"} className="mb-0">
-                <div className="filter-btn-wrap">
-                  <Label className="height17 label" />
-                  <div className="form-group mb-0">
-                    <span className="mr-2">
+              <Col lg={'2'} md={'2'} className='mb-0'>
+                <div className='filter-btn-wrap'>
+                  <Label className='height17 label' />
+                  <div className='form-group mb-0'>
+                    <span className='mr-2'>
                       <button
-                        type="submit"
-                        className="btn btn-primary"
-                        id="Tooltip-1"
+                        type='submit'
+                        className='btn btn-primary'
+                        id='Tooltip-1'
                       >
-                        <i className="fa fa-search" />
+                        <i className='fa fa-search' />
                       </button>
-                      <UncontrolledTooltip target="Tooltip-1">
+                      <UncontrolledTooltip target='Tooltip-1'>
                         Search
                       </UncontrolledTooltip>
                     </span>
-                    <span className="">
+                    <span className=''>
                       <button
-                        type="button"
-                        className="btn btn-danger"
-                        id="Tooltip-2"
+                        type='button'
+                        className='btn btn-danger'
+                        id='Tooltip-2'
                         onClick={this.onReset}
                       >
-                        <i className="fa fa-refresh" />
+                        <i className='fa fa-refresh' />
                       </button>
-                      <UncontrolledTooltip target={"Tooltip-2"}>
+                      <UncontrolledTooltip target={'Tooltip-2'}>
                         Reset all filters
                       </UncontrolledTooltip>
                     </span>
@@ -203,7 +320,33 @@ class CustomerList extends Component {
         <Table responsive bordered>
           <thead>
             <tr>
-              <th>S.NO</th>
+              <th width='90px'>
+                {customers.length ? (
+                  <div className='table-checkbox-wrap'>
+                    <span className='checkboxli checkbox-custom checkbox-default'>
+                      <Input
+                        type='checkbox'
+                        name='checkbox'
+                        id='checkAll'
+                        checked={selectedCustomers.length === customers.length}
+                        onChange={this.handleCheckAllCheckBox}
+                      />
+                      <label className='' htmlFor='checkAll' />
+                    </span>
+                    <Input
+                      className='commonstatus'
+                      type='select'
+                      id='exampleSelect'
+                      onChange={this.handleActionChange}
+                    >
+                      <option value={''}>Select</option>
+                      <option value={'active'}>Active</option>
+                      <option value={'inactive'}>Inactive</option>
+                      <option value={'delete'}>Delete</option>
+                    </Input>
+                  </div>
+                ) : null}
+              </th>
               <th>First Name</th>
               <th>Last Name</th>
               <th>Email</th>
@@ -218,36 +361,61 @@ class CustomerList extends Component {
                 customers.map((user, index) => {
                   return (
                     <tr key={index}>
-                      <td>{(page - 1) * AppConfig.ITEMS_PER_PAGE + index + 1}</td>
-                      <td>{user.firstName || "-"}</td>
-                      <td>{user.lastName || "-"}</td>
-                      <td>{user.email || "-"}</td>
+                      {/* <td>
+                        {(page - 1) * AppConfig.ITEMS_PER_PAGE + index + 1}
+                      </td> */}
                       <td>
-                        {user.createdAt ? formateDate(user.createdAt) : "-"}
+                        <div className='checkbox-custom checkbox-default coloum-checkbox'>
+                          <Input
+                            type='checkbox'
+                            value={user._id}
+                            checked={selectedCustomers.indexOf(user._id) > -1}
+                            name='checkbox'
+                            onChange={this.handleCheckboxChnage}
+                          />
+                          <label htmlFor={user._id}>
+                            {(page - 1) * AppConfig.ITEMS_PER_PAGE + index + 1}.
+                          </label>
+                        </div>
                       </td>
-                    
+                      <td>{user.firstName || '-'}</td>
+                      <td>{user.lastName || '-'}</td>
+                      <td>{user.email || '-'}</td>
                       <td>
-                        {user.status ? (
-                          <Badge color="success">Active</Badge>
-                        ) : (
-                          <Badge color="danger">Inactive</Badge>
-                        )}
+                        {user.createdAt ? formateDate(user.createdAt) : '-'}
+                      </td>
+
+                      <td>
+                        <Button
+                          color={user.status ? 'success' : 'danger'}
+                          onClick={() =>
+                            this.changeStatus(
+                              {
+                                enabled: user.status ? false : true,
+                              },
+                              user._id
+                            )
+                          }
+                          size='sm'
+                        >
+                          {user.status ? 'Active' : 'Inactive'}
+                        </Button>
                       </td>
                       <td>
                         <Button
-                          color={"primary"}
-                          size={"sm"}
+                          color={'primary'}
+                          size={'sm'}
                           onClick={() => this.editUser(user)}
                         >
-                          <i className={"fa fa-edit"} />
-                        </Button>{" "}
+                          <i className={'fa fa-edit'} />
+                        </Button>{' '}
                         &nbsp;
                         <Button
-                          color={"danger"}
-                          size={"sm"}
+                          color={'danger'}
+                          size={'sm'}
                           onClick={() => this.onDelete(user._id)}
                         >
-                          <i className={"fa fa-trash"} />
+                          <i className={'fa fa-trash'} />
                         </Button>
                       </td>
                     </tr>
@@ -255,21 +423,21 @@ class CustomerList extends Component {
                 })
               ) : (
                 <tr>
-                  <td className={"text-center"} colSpan={10}>
+                  <td className={'text-center'} colSpan={10}>
                     No customer records are available
                   </td>
                 </tr>
               )
             ) : (
               <tr>
-                <td className={"text-center"} colSpan={10}>
+                <td className={'text-center'} colSpan={10}>
                   <Loader />
                 </td>
               </tr>
             )}
           </tbody>
         </Table>
-        {totalCustomers && !isLoading ? (          
+        {totalCustomers && !isLoading ? (
           <PaginationHelper
             totalRecords={totalCustomers}
             onPageChanged={page => {
@@ -286,7 +454,7 @@ class CustomerList extends Component {
           handleUserModal={() => {
             this.setState({
               openEditModal: false,
-              user: {}
+              user: {},
             });
           }}
           userData={user}
