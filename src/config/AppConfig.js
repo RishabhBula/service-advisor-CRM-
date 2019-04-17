792d2ad2d7fd3f0b767f0015a35665fd492c5d59
Sonu <sonu.chapter247@gmail.com>
2019-04-17 19:54:48 +0530

    Logo Added

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,6 +1,6 @@
 export const EnviornmentTypes = {
   DEV: "development",
-  PROD: "production",
+  PROD: "production"
 };
 export const mode = process.env.NODE_ENV || EnviornmentTypes.DEV; //stage,dev,live
 export const isProd = mode === EnviornmentTypes.PROD;
@@ -11,5 +11,6 @@ data = {
   phoneLength: 3,
   DEFAULT_DATE_FORMAT: "LLL",
   ITEMS_PER_PAGE: 5,
+  IMAGE_ENDPOINT: process.env.REACT_APP_API_ENDPOINT
 };
 export const AppConfig = data;
