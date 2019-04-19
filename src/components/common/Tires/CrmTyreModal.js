263fbf92b4a1ccbc0a9180f0d152eb2cc68cda21
Rishabh Bula <rishab.chapter247@gmail.com>
2019-04-19 12:33:12 +0530

    changes in tyre modal

--- src/components/common/Tires/CrmTyreModal.js
+++ src/components/common/Tires/CrmTyreModal.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import * as classnames from "classnames";
 import {
    Button,
    Modal,
@@ -9,28 +8,72 @@ import {
    Row,
    Col,
    ButtonGroup,
-   FormFeedback,
    FormGroup,
    Label,
    Input,
 } from 'reactstrap';
-import { PhoneOptions } from '../../../config/Constants';
-import MaskedInput from 'react-maskedinput';
-import { logger } from '../../../helpers/Logger';
-import Validator from 'js-object-validation';
-import { toast } from 'react-toastify';
-import {
-   CreateFleetValidations,
-   CreateFleetValidMessaages,
-} from '../../../validations';
 
 export class CrmTyreModal extends Component {
    constructor(props) {
       super(props);
       this.state = {
+         brandName: "",
+         modalName: "",
+         vendor: "",
+         seasonality: "",
+         tierSize: [
+            {
+               baseInfo: "",
+               notes: "",
+               part: "",
+               bin: "",
+               quantity: "",
+               criticalQuantity: "",
+               priceMatrix: "",
+               cost: "",
+               retailPrice: "",
+               markup: "",
+               margin: ""
+            }
+         ]
       };
    }
 
+   handleAddTierSize = () => {
+      const { tierSize } = this.state;
+      if (tierSize.length < 5) {
+         this.setState((state, props) => {
+            return {
+               tierSize: state.tierSize.concat([
+                  {
+                     baseInfo: "",
+                     notes: "",
+                     part: "",
+                     bin: "",
+                     quantity: "",
+                     criticalQuantity: "",
+                     priceMatrix: "",
+                     cost: "",
+                     retailPrice: "",
+                     markup: "",
+                     margin: ""
+                  },
+               ]),
+            };
+         });
+      }
+   };
+
+   handleRemoveTierSize = index => {
+      const { tierSize } = this.state;
+      let t = [...tierSize];
+      t.splice(index, 1);
+      if (tierSize.length) {
+         this.setState({
+            tierSize: t,
+         });
+      }
+   };
 
    handleAddTire = () => {
 
@@ -41,6 +84,9 @@ export class CrmTyreModal extends Component {
          tyreModalOpen,
          handleTierModal
       } = this.props;
+      const {
+         tierSize
+      } = this.state
       return (
          <>
             <Modal
@@ -90,16 +136,120 @@ export class CrmTyreModal extends Component {
                         </Col>
                      </Row>
                      <div className="">
-                        <ButtonGroup className="tyre-season">
-                           <Button color="info">Summer</Button>
-                           <Button color="info">Winter</Button>
-                           <Button color="info">All Seasons</Button>
-                        </ButtonGroup>
+                        <h6>Seasonality</h6>
+                        <FormGroup>
+                           <ButtonGroup className="tyre-season">
+                              <Button color="info">Summer</Button>
+                              <Button color="info">Winter</Button>
+                              <Button color="info">All Seasons</Button>
+                           </ButtonGroup>
+                        </FormGroup>
                      </div>
+                     <h6>Sizes</h6>
+                     {tierSize && tierSize.length
+                        ? tierSize.map((item, index) => {
+                           return (
+                              <div className="tyre-size-col">
+                                 <Row>
+                                    <Col md="6">
+                                       <h6>Base Info</h6>
+                                       <FormGroup>
+                                          <Input type="text" placeholder={"175/70R13 82T"} />
+                                       </FormGroup>
+                                       <h6>Note</h6>
+                                       <FormGroup>
+                                          <Input type="textarea" col={"2"} row={"5"} placeholder={"175/70R13 82T"} />
+                                       </FormGroup>
+                                       <div className="child-row">
+                                          <Row className="m-0">
+                                             <Col md="6">
+                                                <h6>Part #</h6>
+                                                <FormGroup>
+                                                   <Input type="text" placeholder={"175/70R13 82T"} />
+                                                </FormGroup>
+                                             </Col>
+                                             <Col md="6">
+                                                <h6>Bin</h6>
+                                                <FormGroup>
+                                                   <Input type="text" placeholder={"175/70R13 82T"} />
+                                                </FormGroup>
+                                             </Col>
+                                          </Row>
+                                          <Row className="m-0">
+                                             <Col md="6">
+                                                <h6>Quantity</h6>
+                                                <FormGroup>
+                                                   <Input type="text" placeholder={"175/70R13 82T"} />
+                                                </FormGroup>
+                                             </Col>
+                                             <Col md="6">
+                                                <h6>Critical Quantity</h6>
+                                                <FormGroup>
+                                                   <Input type="text" placeholder={"175/70R13 82T"} />
+                                                </FormGroup>
+                                             </Col>
+                                          </Row>
+                                       </div>
+                                    </Col>
+                                    <Col md="6">
+                                       <h6>Pricing Matrix</h6>
+                                       <FormGroup>
+                                          <Input type="text" placeholder={"175/70R13 82T"} />
+                                       </FormGroup>
+                                       <div className="child-row">
+                                          <Row className="m-0">
+                                             <Col md="6">
+                                                <h6>Cost</h6>
+                                                <FormGroup>
+                                                   <Input type="text" placeholder={"175/70R13 82T"} />
+                                                </FormGroup>
+                                             </Col>
+                                             <Col md="6">
+                                                <h6>Retail Price</h6>
+                                                <FormGroup>
+                                                   <Input type="text" placeholder={"175/70R13 82T"} />
+                                                </FormGroup>
+                                             </Col>
+                                          </Row>
+                                       </div>
+                                       <h6>Markup</h6>
+                                       <FormGroup>
+                                          <ButtonGroup className="tyre-season">
+                                             <Button color="info">15%</Button>
+                                             <Button color="info">25%</Button>
+                                             <Button color="info">35%</Button>
+                                             <Button color="info">50%</Button>
+                                             <Input type={"text"} placeholder="Enter %" />
+                                          </ButtonGroup>
+                                       </FormGroup>
+                                       <h6>Margin</h6>
+                                       <FormGroup>
+                                          <ButtonGroup className="tyre-season">
+                                             <Button color="info">15%</Button>
+                                             <Button color="info">25%</Button>
+                                             <Button color="info">35%</Button>
+                                             <Button color="info">50%</Button>
+                                             <Input type={"text"} placeholder="Enter %" />
+                                          </ButtonGroup>
+                                       </FormGroup>
+                                    </Col>
+                                 </Row>
+                                 <Button className="btn-sm" onClick={()=> this.handleRemoveTierSize(index)}><i className="fas fa-times" /></Button>
+                              </div>
+                           )
+                        }) : null}
+                     {tierSize.length < 5 ? (
+                        <span
+                           onClick={this.handleAddTierSize}
+                           className='customer-add-phone customer-anchor-text customer-click-btn'
+                        >
+                           Add Tier Size
+                      </span>
+                     ) : null}
                   </div>
                </ModalBody>
                <ModalFooter>
-                  <div class="required-fields">*Fields are Required.</div>
+                  <div className="required-fields">*Fields are Required.</div>
                   <Button
                      color='primary'
                      onClick={() =>
