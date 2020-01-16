83f63eb8ca5440040297dd03af2a5af2fa5924bc
Anand Jain <anand.chapter247@gmail.com>
2020-01-16 17:18:27 +0530

    Update authController.js

--- crm-backend/controllers/frontend/authController.js
+++ crm-backend/controllers/frontend/authController.js
@@ -29,7 +29,7 @@ const signUp = async (req, res) => {
     const planExpDate = moment(new Date(), "DD-MM-YYYY").add(30, 'days');
 
     const roleType = await roleModel.findOne({
-      userType: new RegExp("admin", "i")
+      userType: new RegExp("sub-admin", "i")
     });
     let $data = req.body;
     $data.roleType = roleType._id;
